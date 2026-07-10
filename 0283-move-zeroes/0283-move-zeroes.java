1fdaf787f56046e60b000613971bede5d06d3559
pravin Sonwane <171813375+pravin-sonwane-2004@users.noreply.github.com>
2026-07-10 11:29:33 +0530

    Time: 2 ms (92.2%), Space: 47.6 MB (88.02%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,30 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int pointer = 0;
+//         for(int i=0;i<nums.length;i++) {
+//             if( nums[i] != 0) {
+//                 nums[pointer] = nums[i];
+//                 pointer++;
+//             }
+//         }
+//         while(pointer < nums.length) {
+//             nums[pointer] = 0;
+//             pointer++;
+//         }
+//     }
+// }
 class Solution {
     public void moveZeroes(int[] nums) {
-        int pointer = 0;
+        int j =0;
         for(int i=0;i<nums.length;i++) {
-            if( nums[i] != 0) {
-                nums[pointer] = nums[i];
-                pointer++;
+            if(nums[i] !=0) {
+                nums[j] = nums[i];
+                j++;
             }
         }
-        while(pointer < nums.length) {
-            nums[pointer] = 0;
-            pointer++;
+        while(j!=nums.length) {
+            nums[j]=0;
+            j++;
         }
     }
 }
